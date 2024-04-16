6dcc9893acf48e584603f9e58978c893a489ea0e
Johan Bejar <johanbejarc@hotmail.com>
2024-04-15 22:09:37 -0400

    setup dependencies completed

---
+++ src/providers/index.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import type { ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+const Providers = ({ children }: ProvidersProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default Providers;
