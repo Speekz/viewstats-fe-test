21df2acf3cac87bb458989afe0a6660ab139f183
Johan Bejar <johanbejarc@hotmail.com>
2024-04-17 08:15:34 -0400

    added ignore for build

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -17,6 +17,7 @@ const Dashboard = () => {
         </h3>
         <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           {dashboardStats.map((item) => (
+            // @ts-ignore
             <StatsCards item={item} key={item.name} />
           ))}
           <LineChart />
