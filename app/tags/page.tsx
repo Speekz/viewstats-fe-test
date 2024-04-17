42f8255852572fe5c0e6ebb21f03c5000cda04c8
Johan Bejar <johanbejarc@hotmail.com>
2024-04-17 06:43:11 -0400

    created tags page

---
+++ app/tags/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ApplicationShell from "src/components/templates/ApplicationShell";
+
+export const metadata = {
+  title: "Tags",
+};
+
+const Tags = () => {
+  return (
+    <ApplicationShell title="Tags">
+      <div className="text-white">Nothing yet, come back soon!</div>;
+    </ApplicationShell>
+  );
+};
+
+export default Tags;
