06fdfb32eefaddb5dae2766c062ecf760824a283
Johan Bejar <johanbejarc@hotmail.com>
2024-04-17 00:49:41 -0400

    created dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ApplicationShell from "src/components/templates/ApplicationShell";
+
+export const metadata = {
+  title: "Dashboard",
+};
+
+const Dashboard = () => {
+  return (
+    <ApplicationShell>
+      <label>hi</label>
+    </ApplicationShell>
+  );
+};
+
+export default Dashboard;
